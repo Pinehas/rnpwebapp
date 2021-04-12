d37bec40bf664ec6fc0fd15015ebd22ce706fc06
Phinehas <49527566+Pinehas@users.noreply.github.com>
2021-04-12 12:25:59 +0200

    host settings

--- projet/settings.py
+++ projet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '1cjux#vvxgs8wc*gk&b)mg3(nxlqlh2$i2^ncx7ecybwxs#xze'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lastapps.herokuapp.com']
 
 
 # Application definition
